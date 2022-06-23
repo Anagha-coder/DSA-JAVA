05cad27cffe398b90192309000dfe7f355850197
Anagha Desai <71918177+Anagha-coder@users.noreply.github.com>
2022-06-23 11:31:48 +0530

    Update Q8.java

--- Assignment1/src/com/company/Q8.java
+++ Assignment1/src/com/company/Q8.java
@@ -6,7 +6,7 @@ public class Q8 {
     static boolean isPalindrome(String str){
         int i=0 , j= str.length()-1;
 
-        while(i<j){
+        while(i>j){
             if(str.charAt(i) != str.charAt(j))
                 return false;
             i++;
