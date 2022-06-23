c34233bee15c3da053970afb1adf9435070c3dae
Anagha Desai <71918177+Anagha-coder@users.noreply.github.com>
2022-06-23 10:44:07 +0530

    Update Q1.java

--- Assignment1/src/com/company/Q1.java
+++ Assignment1/src/com/company/Q1.java
@@ -6,8 +6,9 @@ public class Q1 {
     public static void main(String[] args) {
         // 1. Write a program to print whether a number is even or odd, also take input from the user.
         Scanner input= new Scanner(System.in);
+        System.out.println("Enter a no: ");
         int a = input.nextInt();
-        System.out.println("Enter a no: "+ a);
+        
 
         if(a % 2 ==0){
             System.out.println("The number "+ a + " is even");
